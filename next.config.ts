0c28f0f5ca934a51371cd7c969b4d117b0c97edb
yunusedev <yunusemregun222@icloud.com>
2025-10-09 12:24:11 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    ignoreDuringBuild: true
+    ignoreDuringBuilds: true
   }
 };
 
